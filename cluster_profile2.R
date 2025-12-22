33187c8ddcef83f1b31de0c7e8817c5e4e134f91
Jair Andrade <jair.albert.andrade@gmail.com>
2025-12-21 21:21:12 -0500

    Update cluster file

--- cluster_profile2.R
+++ cluster_profile2.R
@@ -145,27 +145,51 @@ prof4_objs <- construct_profile(starting_points   = sp_list,
 
 # phi---------------------------------------------------------------------------
 
-# phi_vals <- seq(5.3, 5.8, by = 0.002)
-#
-# prof5_objs <- construct_profile(fixed_vals        = phi_vals,
-#                                 fixed_pos         = 5,
-#                                 MLE_vals          = MLE_vals,
-#                                 titre_data_list   = titre_data_list,
-#                                 age_inf_data_list = age_inf_data_list,
-#                                 final_age_vctr    = final_age_vctr,
-#                                 n_indiv           = 1e5)
+phi_vals <- seq(5.3, 5.8, by = 0.002)
+
+set.seed(2109)
+
+guesses_df <- profile_design(
+  phi = phi_vals,
+  lower  = box[1, c("lambda_1", "lambda_2", "rho", "log_A0", "sd_1", "sd_2")],
+  upper  = box[2, c("lambda_1", "lambda_2", "rho", "log_A0", "sd_1", "sd_2")],
+  nprof  = 10, type = "sobol")
+
+guesses_df <- guesses_df[, colnames]
+
+# starting points(sp)
+sp_list <- transpose(guesses_df)
+
+prof5_objs <- construct_profile(starting_points   = sp_list,
+                                fixed_pos         = 5,
+                                titre_data_list   = titre_data_list,
+                                age_inf_data_list = age_inf_data_list,
+                                final_age_vctr    = final_age_vctr,
+                                n_indiv           = n_indiv)
 
 # sd_1--------------------------------------------------------------------------
 
-# sd_1_vals <- seq(2, 5, by = 0.01)
-#
-# prof6_objs <- construct_profile(fixed_vals        = sd_1_vals,
-#                                 fixed_pos         = 6,
-#                                 MLE_vals          = MLE_vals,
-#                                 titre_data_list   = titre_data_list,
-#                                 age_inf_data_list = age_inf_data_list,
-#                                 final_age_vctr    = final_age_vctr,
-#                                 n_indiv           = 1e5)
+sd_1_vals <- seq(2, 5, by = 0.01)
+
+set.seed(2118)
+
+guesses_df <- profile_design(
+  sd_1   = sd_1_vals,
+  lower  = box[1, c("lambda_1", "lambda_2", "rho", "log_A0", "phi", "sd_2")],
+  upper  = box[2, c("lambda_1", "lambda_2", "rho", "log_A0", "phi", "sd_2")],
+  nprof  = 10, type = "sobol")
+
+guesses_df <- guesses_df[, colnames]
+
+# starting points(sp)
+sp_list <- transpose(guesses_df)
+
+prof6_objs <- construct_profile(starting_points   = sp_list,
+                                fixed_pos         = 6,
+                                titre_data_list   = titre_data_list,
+                                age_inf_data_list = age_inf_data_list,
+                                final_age_vctr    = final_age_vctr,
+                                n_indiv           = n_indiv)
 
 # sd_2--------------------------------------------------------------------------
 
