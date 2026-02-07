20b7ed84a6690caf58d81e0a9cc9ef7594b6fa9d
Jair Andrade <jair.albert.andrade@gmail.com>
2026-02-07 23:30:27 +0000

    Add alternative MLE

--- R_scripts/inference.R
+++ R_scripts/inference.R
@@ -52,7 +52,7 @@ find_MLE <- function(start_list, age_inf_data_list, final_age_vctr, n_indiv)
 
 
 find_MLE_2 <- function(start_list, titre_data_list, age_inf_data_list,
-                     final_age_vctr, n_indiv)
+                     final_age_vctr, n_indiv, ds)
 {
   set.seed(1742)
   seed_vec <- sample.int(1e7, 10)
@@ -61,7 +61,7 @@ find_MLE_2 <- function(start_list, titre_data_list, age_inf_data_list,
 
     iter_id <- start_obj$iter_id
 
-    fn <- str_glue("./saved_objects/inference/two_datasets/MLE/opt_{iter_id}.rds")
+    fn <- str_glue("./saved_objects/inference/{ds}/MLE/opt_{iter_id}.rds")
 
     if(!file.exists(fn))
     {
@@ -149,6 +149,24 @@ get_starting_points <- function(ds)
       nseq =  200)
   }
 
+  if(ds == "alternative")
+  {
+    set.seed(20260207)
+
+    start_df <- sobol_design(
+      lower = c("lambda_1" = 0.01,
+                "lambda_2" = 0.01,
+                "log_A0"   = 0.1,
+                "phi"      = 1,
+                "sd"       = 0.01),
+      upper = c("lambda_1" = 0.25,
+                "lambda_2" = 0.25,
+                "log_A0"   = 4,
+                "phi"      = 10,
+                "sd"       = 10),
+      nseq =  500)
+  }
+
   par_names <- colnames(start_df)
 
 
@@ -217,22 +235,36 @@ log_lik_prob_inf <- function(pars, age_inf_data_list, final_age_vctr, n_indiv,
 log_lik_titre_prob_inf <- function(pars, titre_data_list, age_inf_data_list,
                                    final_age_vctr, n_indiv, seed_vec)
 {
-  # Infection parameters-------------------
+  # FOIs-------------------
   lambda_1 <- inv.logit(pars[[1]])
   lambda_2 <- inv.logit(pars[[2]])
   lambdas  <- c(lambda_1, lambda_2)
-  rho      <- inv.logit(pars[[3]])
+  beta     <- 1
 
   # Decay rate dynamics--------------------
   decay_rate <- get_decay_rate()
 
+  if(length(pars) == 6)
+  {
+    rho      <- inv.logit(pars[[3]])
 
-  # Peak dynamics--------------------------
-  log_A0  <- exp(pars[[4]])
-  phi     <- exp(pars[[5]])
-  beta    <- 1
+    # Peak dynamics--------------------------
+    log_A0  <- exp(pars[[4]])
+    phi     <- exp(pars[[5]])
 
-  sd <- exp(pars[[6]])
+    sd <- exp(pars[[6]])
+  }
+
+  if(length(pars) == 5)
+  {
+    rho <- 0
+
+    # Peak dynamics--------------------------
+    log_A0  <- exp(pars[[3]])
+    phi     <- exp(pars[[4]])
+
+    sd <- exp(pars[[5]])
+  }
 
   # cat("\n---------------")
   # cat("\n lambda 1: ", lambdas[[1]])
@@ -241,6 +273,7 @@ log_lik_titre_prob_inf <- function(pars, titre_data_list, age_inf_data_list,
   # cat("\n log A0: ", log_A0 )
   # cat("\n phi: ", phi)
   # cat("\n sd: ", sd)
+
 
   n_replicates <- length(seed_vec)
 
@@ -257,7 +290,7 @@ log_lik_titre_prob_inf <- function(pars, titre_data_list, age_inf_data_list,
         n_individuals     = n_indiv)
     })
 
-    ll_age_inf <- sapply(1:2, \(cohort_idx) {
+    ll_age_inf_cohorts <- sapply(1:2, \(cohort_idx) {
 
       inf_df       <- inf_list[[cohort_idx]]
       age_inf_data <- age_inf_data_list[[cohort_idx]]
@@ -265,11 +298,16 @@ log_lik_titre_prob_inf <- function(pars, titre_data_list, age_inf_data_list,
       age_tab <- tabulate(inf_df$age + 1, nbins = max(final_age_vctr[[cohort_idx]]) + 1)
       prob_inf_age <- age_tab / n_indiv
 
-      dbinom(x    = age_inf_data$n_infections,
-             size = age_inf_data$n_individuals,
-             prob = prob_inf_age[-c(1, 2)],
-             log  = TRUE) |> sum()
-    }) |> sum()
+      ll_vals <- dbinom(
+        x    = age_inf_data$n_infections,
+        size = age_inf_data$n_individuals,
+        prob = prob_inf_age[-c(1, 2)] + 1e-9,
+        log  = TRUE)
+
+       sum(ll_vals)
+    })
+
+    ll_age_inf <- sum(ll_age_inf_cohorts)
 
     ll_titre <- sapply(1:2, \(cohort_idx) {
 
@@ -305,7 +343,7 @@ log_lik_titre_prob_inf <- function(pars, titre_data_list, age_inf_data_list,
     ll
   })
 
-  #cat("\n Log lik: ", -mean(ll_vals))
+  # cat("\n Log lik: ", -mean(ll_vals))
 
   # Compute mean log-likelihood and MCSE
   mean_ll <- -mean(ll_vals)            # negative log-likelihood
---
+++ cluster_MLE_3.R
@@ -0,0 +1,32 @@
+library(boot)
+library(flavipack)
+library(future)
+library(furrr)
+library(dplyr)
+library(lubridate)
+library(nloptr)
+library(pomp)
+library(purrr)
+library(readr)
+library(stringr)
+library(tidyr)
+
+source("./R_scripts/DGP.R")
+source("./R_scripts/inference.R")
+
+
+data_obj <- get_inference_data()
+
+age_inf_data_list <- data_obj$age_inf_data_list
+titre_data_list   <- data_obj$titre_data_list
+final_age_vctr    <- data_obj$final_age_vctr
+#-------------------------------------------------------------------------------
+
+start_list <- get_starting_points("alternative")
+
+plan(multisession, workers = availableCores())
+
+res_list <- find_MLE_2(start_list, titre_data_list,
+                       age_inf_data_list, final_age_vctr,
+                       n_indiv = 1e4,
+                       ds = "alternative")
