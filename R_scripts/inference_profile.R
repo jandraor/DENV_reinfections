05d2ac967316a98e98f6efab235b616225bf476d
Jair Andrade <jair.albert.andrade@gmail.com>
2026-04-18 13:29:56 +0100

    Add code for profile likelihood alternative

--- R_scripts/inference_profile.R
+++ R_scripts/inference_profile.R
@@ -109,6 +109,12 @@ optimise_over_fixed_value_2 <- function(starting_point, id, titre_data_list,
     dir.create(dirname(fn), recursive = TRUE, showWarnings = FALSE)
     saveRDS(res, fn)
 
+    if (file.exists(fn)) {
+      message(str_glue("Finished optimisation {id} (fixed_pos = {fixed_pos}) -> saved to {fn}"))
+    } else {
+      warning(str_glue("Optimisation {id} finished but file was NOT written: {fn}"))
+    }
+
   } else res <- readRDS(fn)
 
   res
@@ -116,7 +122,7 @@ optimise_over_fixed_value_2 <- function(starting_point, id, titre_data_list,
 
 construct_profile <- function(starting_points, fixed_pos, titre_data_list,
                               age_inf_data_list, final_age_vctr, n_indiv,
-                              ds)
+                              ds, computation = "parallel")
 {
   if(ds == "one_dataset")
   {
@@ -144,15 +150,24 @@ construct_profile <- function(starting_points, fixed_pos, titre_data_list,
 
   if(ds == "alternative")
   {
-    profile_res <- future_imap(
-      starting_points,
-      optimise_over_fixed_value_2,
-      titre_data_list   = titre_data_list,
-      age_inf_data_list = age_inf_data_list,
-      final_age_vctr    = final_age_vctr,
-      fixed_pos         = fixed_pos,
-      n_indiv           = n_indiv,
-      ds                = ds)
+    arg_list <- list(.x = starting_points,
+                     .f = optimise_over_fixed_value_2,
+                     titre_data_list = titre_data_list,
+                     age_inf_data_list = age_inf_data_list,
+                     final_age_vctr    = final_age_vctr,
+                     fixed_pos         = fixed_pos,
+                     n_indiv           = n_indiv,
+                     ds                = ds)
+
+    if(computation == "parallel")
+    {
+      profile_res <- do.call(future_imap, arg_list)
+    }
+
+    if(computation == "sequential")
+    {
+      profile_res <- do.call(imap, arg_list)
+    }
   }
 
   profile_res
@@ -184,4 +199,107 @@ format_profile_df <- function(obj_list, vals)
   param_profile_df <- param_profile_df[, c(colnames, "ll")]
 
   param_profile_df
+}
+
+alternative_profile <- function(param,
+                                titre_data_list,
+                                age_inf_data_list,
+                                final_age_vctr,
+                                n_indiv,
+                                box,
+                                computation = "parallel")
+{
+  arg_list <- list(titre_data_list   = titre_data_list,
+                   age_inf_data_list = age_inf_data_list,
+                   final_age_vctr    = final_age_vctr,
+                   n_indiv           = n_indiv,
+                   ds                = "alternative",
+                   computation       = computation)
+
+  param_names <- c("lambda_1", "lambda_2", "log_A0", "phi", "sd")
+
+  if (!param %in% param_names) {
+    stop(paste0(param," is an invalid parameter name."))
+  }
+
+  if(param == "lambda_1")
+  {
+    grid_vals <- seq(0.038, 0.04, by = 0.00001)
+
+    set.seed(1656)
+
+    profile_design(
+      lambda_1  = grid_vals,
+      lower     = box[1, setdiff(param_names, param)],
+      upper     = box[2, setdiff(param_names, param)],
+      nprof     = 20,
+      type      = "sobol") -> guesses_df
+  }
+
+  if(param == "lambda_2")
+  {
+    grid_vals <- seq(0.05, 0.07, by = 0.00025)
+
+    set.seed(0954)
+
+    guesses_df <- profile_design(
+      lambda_2  = grid_vals,
+      lower = box[1, setdiff(param_names, param)],
+      upper = box[2, setdiff(param_names, param)],
+      nprof = 20, type = "sobol")
+  }
+
+  if(param == "log_A0")
+  {
+    grid_vals <- seq(1.6, 2.0, by = 0.0025)
+
+    set.seed(0836)
+
+    guesses_df <- profile_design(
+      log_A0 = grid_vals,
+      lower  = box[1, setdiff(param_names, param)],
+      upper  = box[2, setdiff(param_names, param)],
+      nprof  = 20, type = "sobol")
+  }
+
+  if(param == "phi")
+  {
+    grid_vals <- seq(6.6, 7.2, by = 0.01)
+
+    set.seed(2109)
+
+    guesses_df <- profile_design(
+      phi    = grid_vals,
+      lower  = box[1, setdiff(param_names, param)],
+      upper  = box[2, setdiff(param_names, param)],
+      nprof  = 20, type = "sobol")
+  }
+
+  if(param == "sd")
+  {
+    grid_vals <- seq(2.2, 2.9, by = 0.01)
+
+    set.seed(2118)
+
+    guesses_df <- profile_design(
+      sd     = grid_vals,
+      lower  = box[1, setdiff(param_names, param)],
+      upper  = box[2, setdiff(param_names, param)],
+      nprof  = 20, type = "sobol")
+  }
+
+  message(str_glue("...Constructing profile for {param}..."))
+
+  guesses_df <- guesses_df[, param_names]
+
+  # starting points(sp)
+  sp_list <- transpose(guesses_df)
+
+  arg_list$fixed_pos       <- which(param_names %in% param)
+  arg_list$starting_points <- sp_list
+
+  prof_objs <- do.call(construct_profile, arg_list)
+
+  list(prof_objs   = prof_objs,
+       grid_values = guesses_df[[param]])
 }
